a8667a957acbc65d8ce49b0fff9096056df84a26
John Titor <root@localhost>
2021-06-20 15:46:35 +0000

    Rebuild site with 11ty v0.12.1

--- js/instant.page.js
+++ js/instant.page.js
@@ -1,26 +1,108 @@
-/*! instant.page v1.3.1 - (C) 2019 Alexandre Dieulot - https://instant.page/license */
+/*! instant.page v5.1.0 - (C) 2019-2020 Alexandre Dieulot - https://instant.page/license */
 
-let urlToPreload
 let mouseoverTimer
 let lastTouchTimestamp
-
-const prefetcher = document.createElement('link')
-const isSupported = prefetcher.relList && prefetcher.relList.supports && prefetcher.relList.supports('prefetch')
-const isDataSaverEnabled = navigator.connection && navigator.connection.saveData
+const prefetches = new Set()
+const prefetchElement = document.createElement('link')
+const isSupported = prefetchElement.relList && prefetchElement.relList.supports && prefetchElement.relList.supports('prefetch')
+                    && window.IntersectionObserver && 'isIntersecting' in IntersectionObserverEntry.prototype
 const allowQueryString = 'instantAllowQueryString' in document.body.dataset
 const allowExternalLinks = 'instantAllowExternalLinks' in document.body.dataset
 const useWhitelist = 'instantWhitelist' in document.body.dataset
+const mousedownShortcut = 'instantMousedownShortcut' in document.body.dataset
+const DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION = 1111
 
-if (isSupported && !isDataSaverEnabled) {
-  prefetcher.rel = 'prefetch'
-  document.head.appendChild(prefetcher)
+let delayOnHover = 65
+let useMousedown = false
+let useMousedownOnly = false
+let useViewport = false
 
+if ('instantIntensity' in document.body.dataset) {
+  const intensity = document.body.dataset.instantIntensity
+
+  if (intensity.substr(0, 'mousedown'.length) == 'mousedown') {
+    useMousedown = true
+    if (intensity == 'mousedown-only') {
+      useMousedownOnly = true
+    }
+  }
+  else if (intensity.substr(0, 'viewport'.length) == 'viewport') {
+    if (!(navigator.connection && (navigator.connection.saveData || (navigator.connection.effectiveType && navigator.connection.effectiveType.includes('2g'))))) {
+      if (intensity == "viewport") {
+        /* Biggest iPhone resolution (which we want): 414 × 896 = 370944
+         * Small 7" tablet resolution (which we don’t want): 600 × 1024 = 614400
+         * Note that the viewport (which we check here) is smaller than the resolution due to the UI’s chrome */
+        if (document.documentElement.clientWidth * document.documentElement.clientHeight < 450000) {
+          useViewport = true
+        }
+      }
+      else if (intensity == "viewport-all") {
+        useViewport = true
+      }
+    }
+  }
+  else {
+    const milliseconds = parseInt(intensity)
+    if (!isNaN(milliseconds)) {
+      delayOnHover = milliseconds
+    }
+  }
+}
+
+if (isSupported) {
   const eventListenersOptions = {
     capture: true,
     passive: true,
   }
-  document.addEventListener('touchstart', touchstartListener, eventListenersOptions)
-  document.addEventListener('mouseover', mouseoverListener, eventListenersOptions)
+
+  if (!useMousedownOnly) {
+    document.addEventListener('touchstart', touchstartListener, eventListenersOptions)
+  }
+
+  if (!useMousedown) {
+    document.addEventListener('mouseover', mouseoverListener, eventListenersOptions)
+  }
+  else if (!mousedownShortcut) {
+      document.addEventListener('mousedown', mousedownListener, eventListenersOptions)
+  }
+
+  if (mousedownShortcut) {
+    document.addEventListener('mousedown', mousedownShortcutListener, eventListenersOptions)
+  }
+
+  if (useViewport) {
+    let triggeringFunction
+    if (window.requestIdleCallback) {
+      triggeringFunction = (callback) => {
+        requestIdleCallback(callback, {
+          timeout: 1500,
+        })
+      }
+    }
+    else {
+      triggeringFunction = (callback) => {
+        callback()
+      }
+    }
+
+    triggeringFunction(() => {
+      const intersectionObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const linkElement = entry.target
+            intersectionObserver.unobserve(linkElement)
+            preload(linkElement.href)
+          }
+        })
+      })
+
+      document.querySelectorAll('a').forEach((linkElement) => {
+        if (isPreloadable(linkElement)) {
+          intersectionObserver.observe(linkElement)
+        }
+      })
+    })
+  }
 }
 
 function touchstartListener(event) {
@@ -34,20 +116,11 @@ function touchstartListener(event) {
     return
   }
 
-  linkElement.addEventListener('touchcancel', touchendAndTouchcancelListener, {passive: true})
-  linkElement.addEventListener('touchend', touchendAndTouchcancelListener, {passive: true})
-
-  urlToPreload = linkElement.href
   preload(linkElement.href)
 }
 
-function touchendAndTouchcancelListener() {
-  urlToPreload = undefined
-  stopPreloading()
-}
-
 function mouseoverListener(event) {
-  if (performance.now() - lastTouchTimestamp < 1100) {
+  if (performance.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
     return
   }
 
@@ -59,12 +132,20 @@ function mouseoverListener(event) {
 
   linkElement.addEventListener('mouseout', mouseoutListener, {passive: true})
 
-  urlToPreload = linkElement.href
-
   mouseoverTimer = setTimeout(() => {
     preload(linkElement.href)
     mouseoverTimer = undefined
-  }, 65)
+  }, delayOnHover)
+}
+
+function mousedownListener(event) {
+  const linkElement = event.target.closest('a')
+
+  if (!isPreloadable(linkElement)) {
+    return
+  }
+
+  preload(linkElement.href)
 }
 
 function mouseoutListener(event) {
@@ -76,18 +157,37 @@ function mouseoutListener(event) {
     clearTimeout(mouseoverTimer)
     mouseoverTimer = undefined
   }
-  else {
-    urlToPreload = undefined
-    stopPreloading()
+}
+
+function mousedownShortcutListener(event) {
+  if (performance.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
+    return
   }
+
+  const linkElement = event.target.closest('a')
+
+  if (event.which > 1 || event.metaKey || event.ctrlKey) {
+    return
+  }
+
+  if (!linkElement) {
+    return
+  }
+
+  linkElement.addEventListener('click', function (event) {
+    if (event.detail == 1337) {
+      return
+    }
+
+    event.preventDefault()
+  }, {capture: true, passive: false, once: true})
+
+  const customEvent = new MouseEvent('click', {view: window, bubbles: true, cancelable: false, detail: 1337})
+  linkElement.dispatchEvent(customEvent)
 }
 
 function isPreloadable(linkElement) {
   if (!linkElement || !linkElement.href) {
-    return
-  }
-
-  if (urlToPreload == linkElement.href) {
     return
   }
 
@@ -123,9 +223,14 @@ function isPreloadable(linkElement) {
 }
 
 function preload(url) {
-  prefetcher.href = url
-}
+  if (prefetches.has(url)) {
+    return
+  }
 
-function stopPreloading() {
-  prefetcher.removeAttribute('href')
+  const prefetcher = document.createElement('link')
+  prefetcher.rel = 'prefetch'
+  prefetcher.href = url
+  document.head.appendChild(prefetcher)
+
+  prefetches.add(url)
 }
